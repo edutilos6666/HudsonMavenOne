7889b0f8f1608e3119fd2f904363d1f317172f5f
edutilos666 <edutilosaghayev@yahoo.com>
2015-06-23 23:36:00 +0500

    : ProjectForFindBugs Task
    Task-Url:

---
+++ MavenHudson1/src/test/java/com/edutilos/groovy/EmployeeTest.java
@@ -0,0 +1,29 @@
+package com.edutilos.groovy;
+
+
+import static org.junit.Assert.*; 
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class EmployeeTest {
+   @Before
+   public void setup() {
+	   System.out.println(getClass().getName()+ " setup");
+   }
+   
+   @After
+   public void tearDown() {
+	   System.out.println(getClass().getName()+ " tearDown");
+   }
+   
+   @Test
+   public void testMethod1() {
+	 Employee emp = new Employee(1, "foobar", 10); 
+	 assertEquals(1, emp.getId());
+	 assertEquals("foobar", emp.getName()); 
+	 assertEquals(10, emp.getAge()); 
+   }
+}
